de2f0ceab06a738c209b5bed510d7f3fda85bae8
Sai Kiran Pothula <saikiranrede93@gmail.com>
2018-04-08 00:52:59 -0700

    db conns and queries with sqlite3 and postgres

---
+++ dbConnEx/sqlite3Db.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+def create_table():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price FLOAT)")
+    conn.commit()
+    conn.close()
+
+def insert(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?, ?, ?)", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+#insert("Water", 10, 5)
+#insert("Coffee", 10, 10)
+
+def show():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * from store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?", (item,))
+    conn.commit()
+    conn.close()
+
+def update(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+update("Coffee", 12, 8.5)
+#delete("Water")
+print(show())
